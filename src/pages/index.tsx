1bdffc510d372aa8f0383aacdb1e01b942ff8e73
Steve Reid <steve@appby.design>
2021-04-09 17:49:21 +0100

    initial comit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Create Next App Starter</h1>
+    </div>
+  );
+}
